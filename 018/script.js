d5a93590b97864b20f6b7e2cdeede393ff0585a3
dixter970 <145441173+dixter970@users.noreply.github.com>
2023-11-13 12:20:35 +0100

    Add files via upload

---
+++ 018/script.js
@@ -0,0 +1,14 @@
+const list = []
+
+function listAdd() {
+    const item = document.getElementById("input").value
+
+    list.push(item)
+    document.getElementById("list").innerHTML = list;
+
+
+}
+function deleteItem() {
+    const 
+    
+    }
