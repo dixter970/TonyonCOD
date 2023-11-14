9b956474440498881c27b5247025c3039d8700d8
dixter970 <145441173+dixter970@users.noreply.github.com>
2023-11-14 13:12:00 +0100

    14.11.2023

---
+++ 019/script.js
@@ -0,0 +1,21 @@
+let btn = document.getElementById("btn");
+let output = document.getElementById("output");
+let quotes = [
+   "Když procházíš peklem, nezastavuj se.",
+"Buďte laskaví vždy, když je to možné. Vždy je to možné",
+"Buď sám tou změnou, kterou chceš vidět ve světě.",
+"Vše, co si dokážete představit je skutečné.",
+"Žij, jako bys měl zítra zemřít. Uč se, jako bys měl navždy žít.",
+"Jestli najdeš v životě cestu bez překážek, určitě nikam nevede.",
+"Je lepší zemřít pro něco než žít pro nic.",
+"Celý život je experiment. Čím více experimentů uděláte, tím lépe.",
+"Nenáviděl jsem každou minutu tréninku, ale vždy jsem si říkal: Teď protrpíš trénink a žij zbytek života jako mistr.",
+"Mnoho životních zklamání je od lidí, co nezjistili, jak blízko byli úspěchu, kdyby to nevzdali.",
+"Když něco opravdu chceš, celý vesmír se spojí, abys to mohl uskutečnit.",
+"Giving up is gay/ sensei WU"
+];
+btn.addEventListener("click", function() {
+    var randomQuote = quotes[Math.floor(Math.random()* quotes.length)]
+    output.innerHTML = randomQuote;
+    
+})
